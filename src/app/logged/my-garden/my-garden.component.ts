f97ceffb7c61d37ab773f7c9080f52fdd5a7e57b
EliseLabarrere <lab.elise@outlook.fr>
2024-05-03 01:45:25 +0200

    feat: sections of garden page

--- src/app/logged/my-garden/my-garden.component.ts
+++ src/app/logged/my-garden/my-garden.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../shared/services/api.service';
 
 @Component({
   selector: 'app-my-garden',
   templateUrl: './my-garden.component.html',
-  styleUrl: './my-garden.component.scss'
+  styleUrl: './my-garden.component.scss',
 })
 export class MyGardenComponent {
+  allMyPlants: any;
+  wateringPlants: any;
+  haveToWatering: boolean = true;
 
+  constructor(
+    public apiService: ApiService,
+  ) {
+    this.apiService.requestApi('/api/user/plantInGarden/').then(
+      (data) => {
+        this.allMyPlants = data.plantsInGarden
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    this.apiService.requestApi('/api/task/checkDailyTask/').then(
+      (data) => {
+        console.log('daily tasks', data.status)
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    let today = new Date();
+    this.apiService.requestApi('/api/task/one', 'POST', { day: today}).then(
+      (data) => {
+        this.wateringPlants = data.wateringPlants;
+        this.wateringPlants.length > 0 ? this.haveToWatering = true : false;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
